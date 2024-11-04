e613e47ddf737c81315ce06799080261b7c44e5a
mansinighot93 <mansinighot93@gmail.com>
2024-11-04 16:04:36 +0530

    changes

--- FirstWebApp/HelloApp/views.py
+++ FirstWebApp/HelloApp/views.py
@@ -5,3 +5,13 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Welcome To Transflower")
+
+def about(request):
+    return HttpResponse("Transflower pvt.ltd")
+
+def contact(request):
+    return HttpResponse("Manchar")
+
+    
+
+
